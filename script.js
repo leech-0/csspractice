8f8923f6544b12ed9ca2cd9519dbd8b1abed80ec
leech-0 <121104747+leech-0@users.noreply.github.com>
2023-01-14 17:39:33 +0900

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var inputData, dataLength,num;
-inputData = prompt("이게 css로 가능하다고?");
-const inputArr = Array.from(inputData);
-dataLength=inputArr.length;
-for(num=0;num<dataLength;num++){
-    document.write("<span>"+inputArr[num]+"</span>");
-}
